d151f051258e5ef9e73b234dd3057f6529943863
Jon Goodger <jonno.is@gmail.com>
2023-08-07 14:32:01 +0100

    Fix error when AUTO_PREPEND_FILE is not set

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 prepend_file_path=/usr/local/etc/php/conf.d/00-auto_prepend_file.ini
 
 # If we have a prepend file set as an env var, set that up
-if [ "x${AUTO_PREPEND_FILE}" != "x" ]; then
+if [ "x${AUTO_PREPEND_FILE:-}" != "x" ]; then
     echo "auto_prepend_file=\"$AUTO_PREPEND_FILE\"" > $prepend_file_path
 fi
 
